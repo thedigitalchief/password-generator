c9687d48a936b698f9e43c90780ce23d4ac545db
thedigitalchief <me@dylanhnguyen.com>
2022-06-05 15:48:28 -0700

    added shuffling characters to fill in missing length elements

--- generator.py
+++ generator.py
@@ -10,18 +10,15 @@ characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 
 def generate_random_password():
-<<<<<<< HEAD
     #length of password from the user
     length = int(input("Enter password length: "))
 
-
     #asking for user inputs
-alphabets_count = int(input("Enter alphabets count in password: "))
+	alphabets_count = int(input("Enter alphabets count in password: "))
 	digits_count = int(input("Enter digits count in password: "))
-	special_characters_count = int(
-	    input("Enter special characters count in password: "))
+	special_characters_count = int(input("Enter special characters count in password: "))
 
-    characters_count = alphabets_count + digits_count + special_characters_count
+    	characters_count = alphabets_count + digits_count + special_characters_count
 
     #checking if total length with characters sum count, prints "not valid" if the sum of char > length
     if characters_count > length:
@@ -43,11 +40,15 @@ alphabets_count = int(input("Enter alphabets count in password: "))
 	for i in range(special_characters_count):
 		pw.append(random.choice(special_characters))
 
-    if characters_count < length:
-   		random.shuffle(characters)
+    #if total character count < password length: add random characters to make it equal to the length
+	if characters_count < length:
+		random.shuffle(characters)
+		for i in range(length - characters_count):
+			pw.append(random.choice(characters))
+		#shuffling the resulting generated password
+  
+	random.shuffle(pw)
+	
 
 
 generate_random_password()
-=======
-    
->>>>>>> 2fd17c6ba0e5521d77eb2eb56fe5110303e31597
