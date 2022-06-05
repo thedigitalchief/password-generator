b3d3efa585e2ff277f5ae3662890955f38d76079
thedigitalchief <me@dylanhnguyen.com>
2022-06-05 15:37:42 -0700

    finalized main password generating function

--- generator.py
+++ generator.py
@@ -12,22 +12,24 @@ characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 def generate_random_password():
     #length of password from the user
     length = int(input("Enter password length: "))
- 
+
+
     #asking for user inputs
-	alphabets_count = int(input("Enter alphabets count in password: "))
+alphabets_count = int(input("Enter alphabets count in password: "))
 	digits_count = int(input("Enter digits count in password: "))
-	special_characters_count = int(input("Enter special characters count in password: "))
- 
+	special_characters_count = int(
+	    input("Enter special characters count in password: "))
+
     characters_count = alphabets_count + digits_count + special_characters_count
-    
+
     #checking if total length with characters sum count, prints "not valid" if the sum of char > length
     if characters_count > length:
         print("Invalid. Characters total count is greater than the password length.")
         return
-    
+
     #initializing pw
 	pw = []
- 
+
     #picking random alphabet letters
 	for i in range(alphabets_count):
 		pw.append(random.choice(alphabets))
@@ -39,11 +41,9 @@ def generate_random_password():
 	#picking random special characters
 	for i in range(special_characters_count):
 		pw.append(random.choice(special_characters))
-  
-    if characters_count < length:
-    		random.shuffle(characters)
-    
-    
-generate_random_password()
-    
 
+    if characters_count < length:
+   		random.shuffle(characters)
+
+
+generate_random_password()
