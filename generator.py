7d3fb52b875e341f0f784f0f6cf03a1ad0190e23
thedigitalchief <me@dylanhnguyen.com>
2022-06-05 11:34:47 -0700

    added prompts for user input for amount of letters, digits, and special characters

--- generator.py
+++ generator.py
@@ -10,6 +10,11 @@ characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 
 def generate_random_password():
-    
     #length of password from the user
 	length = int(input("Enter password length: "))
+ 
+    #asking for user inputs
+	alphabets_count = int(input("Enter alphabets count in password: "))
+	digits_count = int(input("Enter digits count in password: "))
+	special_characters_count = int(input("Enter special characters count in password: "))
+
