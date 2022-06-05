5e20283ee11795f7d40b0128df2765741b473e55
thedigitalchief <me@dylanhnguyen.com>
2022-06-05 11:45:45 -0700

    added if statement to catch invalid user inputs and a character length counter

--- generator.py
+++ generator.py
@@ -11,10 +11,18 @@ characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 def generate_random_password():
     #length of password from the user
-	length = int(input("Enter password length: "))
+    length = int(input("Enter password length: "))
  
     #asking for user inputs
 	alphabets_count = int(input("Enter alphabets count in password: "))
 	digits_count = int(input("Enter digits count in password: "))
 	special_characters_count = int(input("Enter special characters count in password: "))
+ 
+    characters_count = alphabets_count + digits_count + special_characters_count
+    
+    #checking if total length with characters sum count, prints "not valid" if the sum of char > length
+    if characters_count > length:
+        print("Invalid. Characters total count is greater than the password length.")
+        return
+    
 
